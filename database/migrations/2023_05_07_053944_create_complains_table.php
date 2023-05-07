1abba6a279db583be5e34cf13d36bb5062118659
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-05-07 11:45:52 +0530

    made migrations for complain table

--- database/migrations/2023_05_07_053944_create_complains_table.php
+++ database/migrations/2023_05_07_053944_create_complains_table.php
@@ -13,6 +13,35 @@ return new class extends Migration
     {
         Schema::create('complains', function (Blueprint $table) {
             $table->id();
+
+            //Submitting complain
+            $table->unsignedBigInteger('user_id');
+            $table->string('department');
+            $table->unsignedBigInteger('room_no');
+            $table->string('reported_by');
+            $table->string('requested_by');
+            $table->string('description');
+
+            //Feedback
+            $table->string('comp_feedback')->nullable();
+            $table->string('comp_comment')->nullable();
+            
+            //Assignee form
+            $table->string('asignee_comment')->nullable();
+            $table->date('date_of_completion')->nullable();
+            
+
+            //Admin action for complain
+                //assinging complain
+            $table->string('assigned_to')->nullable();
+            $table->string('admin_remarks')->nullable();
+                //closing complain
+            $table->string('admin_comment')->nullable();
+
+            //complain status, not closed is 0
+            $table->boolean('complain_status')->default(0);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
